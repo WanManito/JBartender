c8280f38dc0b01f47af6a4cb82cd5a1241186fd5
wt <2430937882@qq.com>
2019-08-12 17:18:03 +0800

    提交测试用例

--- src/main/java/com/jna/App.java
+++ src/main/java/com/jna/App.java
@@ -1,13 +1,26 @@
 package com.jna;
 
+import com.jna.bartender.JBartender;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        JBartender jBartender = new JBartender();
+        if (jBartender.initEngine()) {
+            if (jBartender.openTemplate("C:\\Users\\1234\\Desktop\\test.btw")) {
+                if (jBartender.getKeyCount() > 0) {
+                    jBartender.setSupportsIdenticalCopies(1);
+                    for (int i = 0; i < jBartender.getKeyCount(); i++) {
+                        System.out.println(jBartender.getKeyName(i)+"----"+jBartender.getKeyValue(i));
+                    }
+                    jBartender.closeSave();
+                }
+            }
+        }
     }
 }
